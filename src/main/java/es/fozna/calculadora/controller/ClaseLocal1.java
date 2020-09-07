60e33a2560d472be320d98f12fc630a41cd7de5f
Adolfo Urrutia <adolfo.urrutia@WANNME-L-004.wannme.local>
2020-09-07 16:22:50 +0200

    Creada la clase local 1

---
+++ src/main/java/es/fozna/calculadora/controller/ClaseLocal1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.fozna.calculadora.controller;
+
+/**
+ *
+ * @author adolfo.urrutia
+ */
+public class ClaseLocal1 {
+
+    public ClaseLocal1() {
+    }
+    
+}
